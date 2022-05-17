be595f24fd099b87d095d97e290d6468049f1a8f
Francisco Carvajal <fraco_oxza@outlook.com>
2022-05-17 02:54:48 -0400

    v0.1.1

--- src/bin/im1.rs
+++ src/bin/im1.rs
@@ -2,17 +2,29 @@
 extern crate bmp;
 use bmp::{Image, Pixel};
 
-use std::time::{Duration, Instant};
+use std::time::Instant;
 
 use indicatif::ProgressBar;
+use indicatif::ProgressStyle;
 use malderbrot::*;
 
-const MAX_ITERATION: u64 = 1000;
-const RESOLUTION: u32 = 10000;
+const MAX_ITERATION: u64 = 100000;
+const RESOLUTION: u32 = 27000;
 
 fn main() {
-    // let bar = ProgressBar::new(RESOLUTION as u64 * RESOLUTION as u64);
+    let max = RESOLUTION as u64 * RESOLUTION as u64;
+    let bar = ProgressBar::new(max);
     // bar.set_draw_rate(1);
+    bar.set_style(
+        ProgressStyle::default_bar()
+            .template(&format!(
+                "{{prefix:.bold}}▕{{bar:.{}}}▏{{percent}}% ({{eta}})",
+                "yellow"
+            ))
+            .progress_chars("█▉▊▋▌▍▎▏  "),
+    );
+    bar.set_prefix("Calculating: ");
+    bar.set_draw_rate(4);
     let res_float = RESOLUTION as f64;
     let va = 255.0 / MAX_ITERATION as f64;
     let mut img = Image::new(RESOLUTION, RESOLUTION);
@@ -27,11 +39,12 @@ fn main() {
         );
 
         color = 255 - (va * r as f64) as u8;
-        img.set_pixel(x, y, px!(color, color, color));
-        // bar.inc(1);
+        img.set_pixel(x, y, px!(color, color / 2, color / 3));
+        bar.inc(1);
     }
-    let duration = start.elapsed().as_secs_f32();
-    // bar.finish();
-    println!("Ejecutado en {:.4}s", duration);
+    let duration = start.elapsed().as_secs();
+    bar.finish();
+    println!("Ejecutado en {}s", duration);
+    println!("Saving...");
     img.save("img.bmp").unwrap();
 }
